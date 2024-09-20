e2b7ba7cdb275f36626ef38b8141960af6349f19
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2024-09-20 16:44:01 +0200

    add layouts, main layout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <Link href="/">Index page</Link>&nbsp;
+    <Link href="/hello">Show page</Link>
+    <div>Timer: {{ timer }}</div>
+    <slot></slot>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+    import {ref} from 'vue'
+
+    const timer = ref(0)
+    setInterval(() => timer.value++, 1000)
+</script> 
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,12 @@
+<template>
+        <div>Show</div>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue';
+    defineProps({
+        message: String
+    })
+</script>
+
