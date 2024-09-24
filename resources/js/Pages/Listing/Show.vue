60f1e8e4c70d8dd21509aff56816b5bc3df4982f
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2024-09-25 01:59:04 +0200

    add monthly payment calculator

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col flex-col-reverse md:grid grid-cols-12 gap-4">
+    <div class="flex flex-col-reverse md:grid grid-cols-12 gap-4">
         <Box class="md:col-span-7 flex items-center w-full">
             <div class="w-full text-center text-gray-500">No images</div>
         </Box>
@@ -14,9 +14,18 @@
             </Box>
             <Box>
                 <template #header>
-                    Offer
+                    Monthly Payment
                 </template>
-                Make an offer
+                <div>
+                    <label class="label">Interest rate({{interestRate}}%)</label>
+                    <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1" class="w-full h-4 mb-3 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                    <label class="label">Duration({{duration}} years)</label>
+                    <input v-model.number="duration" type="range" min="3" max="35" step="1" class="w-full h-4 mb-3 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your monthly Payment</div>
+                        <Price :price="monthlyPayment" class="text-3xl"></Price>
+                    </div>
+                </div>
             </Box>
         </div>
     </div>
@@ -27,8 +36,21 @@ import ListingAddress from '@/Components/ListingAddress.vue';
 import Box from '@/Components/UI/Box.vue';
 import ListingSpace from '@/Components/UI/ListingSpace.vue';
 import Price from '@/Components/UI/Price.vue';
+import {ref, computed} from 'vue'
 
-defineProps({
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
     listing: Object
 })
+
+const monthlyPayment = computed(() => {
+    const principle = props.listing.price
+    const monthlyInterest = interestRate.value / 100 / 12
+    const numberOfPaymentMonths = duration.value * 12
+
+    return principle * monthlyInterest * (Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
+})
+
 </script>
