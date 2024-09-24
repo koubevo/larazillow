07b9bb97570e447fb53c67e2d9e62cb2f506bbb7
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2024-09-25 00:37:35 +0200

    add price format

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,6 +3,7 @@
         <Box v-for="listing in listings" :key="listing.id">
         <div>
             <Link :href="route('listing.show', { listing: listing.id })">
+                <Price :price="listing.price" class="text-2xl font-bold"></Price>
                 <ListingSpace :listing="listing" class="text-lg" />
                 <ListingAddress :listing="listing" class="text-grey-500" />
             </Link>
@@ -22,6 +23,7 @@ import ListingAddress from '@/Components/ListingAddress.vue';
 import { Link } from '@inertiajs/vue3';
 import Box from '@/Components/UI/Box.vue';
 import ListingSpace from '@/Components/UI/ListingSpace.vue';
+import Price from '@/Components/UI/Price.vue';
 defineProps({
     listings: Array,
 })
