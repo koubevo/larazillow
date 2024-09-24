e5a6c00a7fec1c02314c44660671fa7164c79d6c
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2024-09-24 23:40:24 +0200

    add styling to mainlayout.vue

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,12 +1,28 @@
 <template>
-    <Link href="/listing">Listings</Link>&nbsp;
-    <Link href="/listing/create">Create Listing</Link>
+    <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full">
+      <div class="container mx-auto">
+        <nav class="p-4 flex items-center justify-between">
+          <div class="text-lg font.medium">
+            <Link :href="route('listing.index')">Listings</Link>
+          </div>
+          <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+            <Link :href="route('listing.index')">Larazillow</Link>
+          </div>
+          <div>
+            <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+          </div>
+        </nav>
+      </div>
+    </header>
 
-    <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
+    <main class="container mx-auto p-4">
+      <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+        {{ flashSuccess }}
+        Iam flash
+      </div>
 
-    <slot></slot>
+      <slot></slot>
+    </main>
 </template>
 
 <script setup>
