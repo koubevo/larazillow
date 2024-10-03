05be3b78c606dee1e2eb874ac61d6795cd8155b7
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2024-10-03 13:53:05 +0200

    add notifications sending

--- app/Notifications/OfferMade.php
+++ app/Notifications/OfferMade.php
@@ -28,7 +28,7 @@ class OfferMade extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -37,8 +37,8 @@ class OfferMade extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
+            ->line("New offer ({$this->offer->amount} €) was made for your listing.")
+            ->action('See your listing', route('realtor.listing.show', ['listing' => $this->offer->listing_id]))
             ->line('Thank you for using our application!');
     }
 
